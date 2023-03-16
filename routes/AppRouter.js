4da9d5bede65984452863a0dec5c3db8e04c834f
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-03-16 10:26:42 -0400

    server.js, routes, controllers folder

---
+++ routes/AppRouter.js
@@ -0,0 +1,12 @@
+const Router = require('express').Router()
+// const UserRouter = require('./UserRouter')
+// const ClassRouter = require('./ClassRouter')
+// const CommentRouter = require('./CommentRouter')
+// const ClassListRouter = require('./ClassListRouter')
+
+// Router.use('/users', UserRouter)
+// Router.use('/classes', ClassRouter)
+// Router.use('/comments', CommentRouter)
+// Router.use('/classlists', ClassListRouter)
+
+module.exports = Router
